7bc4d0ff88f1b25758b6824abfb07cae1563bf5d
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-07-25 14:35:43 +0000

    making billing page

--- index.js
+++ index.js
@@ -33,3 +33,35 @@ function showFooter(event, element) {
     }
   }
 }
+
+console.log(window.location.pathname);
+if (window.location.pathname === "/views/billing.html") {
+  const expiration = document.getElementById("expiration");
+  const ExpYear = document.getElementById("year");
+
+  // Loop from 1 to 12 to create <option> elements
+  for (let month = 1; month <= 12; month++) {
+    const optionMonth = document.createElement("option");
+    optionMonth.value = month;
+    optionMonth.textContent = month;
+    expiration.appendChild(optionMonth);
+  }
+
+  for (let year = 2024; year <= 2029; year++) {
+    const optionYear = document.createElement("option");
+    optionYear.value = year;
+    optionYear.textContent = year;
+    ExpYear.appendChild(optionYear);
+  }
+}
+
+// if (window.location.pathname === "/views/billing.html") {
+//   const year = document.getElementById("year");
+//   // Loop from 2024 to 2029 to create <option> elements
+//   for (let year = 2024; year <= 2029; year++) {
+//     const optionYear = document.createElement("option");
+//     optionYear.value = year;
+//     optionYear.textContent = year;
+//     year.appendChild(optionYear);
+//   }
+// }
